8d10799699d2a0b612bd55e5e5a117b5993397c9
Emily Sellers <emilymelindasellers@gmail.com>
2022-10-03 11:55:45 -0700

    add a pet

--- pet-editor/pet-editor.js
+++ pet-editor/pet-editor.js
@@ -1,7 +1,7 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import '../auth/user.js';
-import { uploadImage } from '../fetch-utils.js';
+import { uploadImage, createPet } from '../fetch-utils.js';
 // > Part A: import upload image
 // > Part B: import fetch to create a pet
 
@@ -39,12 +39,22 @@ petForm.addEventListener('submit', async (e) => {
 
     const pet = {
         // > Part B: add the name, bio, and image_url fields to the pet object
+        name: formData.get('name'),
+        bio: formData.get('bio'),
+        image_url: url,
     };
 
     // > Part B:
     //    - call function to create the pet in the database
+    const response = await createPet(pet);
     //    - store the error and pets state from the response
+    error = response.error;
     //    - either display the error or redirect the user to the home page
+    if (error) {
+        displayError();
+    } else {
+        location.assign('/');
+    }
 });
 
 /* Display Functions */
